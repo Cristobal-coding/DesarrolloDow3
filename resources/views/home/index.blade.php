abb70fe78ee5c24acf8fa1c3d0118200c56b461e
Cristobal-Coding <agartkg95@gmail.com>
2020-12-21 22:23:34 -0600

    add Boostrap

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts/master')
 
 @section('main_content')
-<h5>hola kbros qliaos</h5>
+<div class="col d-flex justify-content-center align-items-center vh-100">
+    <button class="btn btn-outline-primary">Add Renato</button>
+</div>
 @endsection
