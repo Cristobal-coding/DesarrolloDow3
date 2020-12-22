123e3a50f731b97de4ed858bb8e2acfc8805ff9c
nattohd <tubepika@gmail.com>
2020-12-21 22:28:00 -0300

    Boton circobol

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,5 +3,6 @@
 @section('main_content')
 <div class="col d-flex justify-content-center align-items-center vh-100">
     <button class="btn btn-outline-primary">Add Renato</button>
+    <button class="btn btn-outline-primary">Add cricobol</button>
 </div>
 @endsection
