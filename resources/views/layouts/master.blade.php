e383d6028cb195b8a9d67890c38c566eda49507c
Cristobal-Coding <agartkg95@gmail.com>
2020-12-23 17:44:15 -0600

    Segments Active

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,61 +6,50 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset("css/theme.css")}}">
+    <script src="https://kit.fontawesome.com/bce530116a.js" crossorigin="anonymous"></script>
     @yield('css-personalizado')
     <title>Cars</title>
 </head>
 <body class="no-scroll">
     <div class="container-fluid m-0 p-0">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary w-100">
+          <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
             <div class="container-fluid">
               <a class="navbar-brand" href="#">Navbar</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link @if(Request::segments()[0]=="home") active @endif" aria-current="page" href="#">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Vehículos</a>
+                    <a class="nav-link @if(Route::current()->getName()!="home.index" && Request::segments()[0]=="autos") active @endif" href="{{route("autos.index")}}">Vehiculos</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Arriendos</a>
+                    <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Arriendos</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Configuración
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
                   </li>
                 </ul>
+                <form class="d-flex justify-content-center align-items-center">
+                  <p class="m-0 p-0 text-light mr-1" style="font-weight: bold; font-size:18px">Pepito01</p>
+                  <button type="button" class="btn btn-primary border-0" data-toggle="tooltip" data-placement="bottom" title="Foto Usuario"><i class="far fa-user fa-lg"></i></button>
+                  <button type="submit" class="btn btn-primary border-0" data-toggle="tooltip" data-placement="bottom" title="Cerrar Sesión"><i class="fas fa-sign-out-alt fa-lg"></i></button>
+                </form>
               </div>
             </div>
           </nav>
-          {{-- <ul class="nav justify-content-center bg-primary">
-            <li class="nav-item ">
-              <a class="nav-link active text-light" aria-current="page" href="#">Active</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link text-light" href="#">Link</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link text-light" href="#">Link</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link disabled text-light" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link disabled text-light" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link disabled text-light" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link disabled text-light" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link disabled text-light" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul> --}}
+  
 
         <div class="row m-0">
             @yield('main_content') 
