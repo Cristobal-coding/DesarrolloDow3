af2dde3f72f125fd7e759bc076573bca4b00754a
Cristobal-Coding <agartkg95@gmail.com>
2020-12-24 15:12:06 -0600

    modify Home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="{{asset("css/myThemes.css")}}">
 @endsection
 @section('main_content')
-<div class="col d-flex justify-content-center align-items-start img-home vh-100">
+<div class="col d-flex justify-content-center align-items-start img-home ">
     <section class="py-5 text-center text-light" >
         <div class="row py-lg-5">
           <div class="col-lg-6 col-md-8 mx-auto">
@@ -15,6 +15,24 @@
           </div>
         </div>
       </section>
-
+      
+</div>
+<div class="col-12 w-100 mx-0 px-0">
+  <div class="d-md-flex flex-md-equal w-100">
+    <div class="bg-dark mx-0 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+      <div class="my-3 py-3">
+        <h2 class="display-5">Another headline</h2>
+        <p class="lead">And an even wittier subheading.</p>
+      </div>
+      <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+    </div>
+    <div class="bg-secondary mx-0 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+      <div class="my-3 p-3">
+        <h2 class="display-5">Another headline</h2>
+        <p class="lead">And an even wittier subheading.</p>
+      </div>
+      <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+    </div>
+  </div>
 </div>
 @endsection
