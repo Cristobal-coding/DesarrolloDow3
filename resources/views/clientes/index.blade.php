0da63a55a6bcec9d6c61797a52ec44065b4dbd90
nattohd <tubepika@gmail.com>
2020-12-25 20:21:35 -0300

    a

---
+++ resources/views/clientes/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/master')
+@section('css-personalizado')
+    <link rel="stylesheet" href="{{asset("css/myThemes.css")}}">
+@endsection
+@section('main_content')
+<div class="col-8 ">
+    
+</div>
+
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -31,7 +31,7 @@
                     <a class="nav-link @if(Route::current()->getName()!="home.index" && Request::segments()[0]=="arriendos") active @endif" href="{{route("arriendos.index")}}" tabindex="-1" aria-disabled="true">Arriendos</a>
                   </li>                  
                   <li class="nav-item">
-                    <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Clientes</a>
+                    <a class="nav-link  @if(Route::current()->getName()!="home.index" && Request::segments()[0]=="clientes") active @endif" href="{{route("clientes.index")}}" tabindex="-1" aria-disabled="true">Clientes</a>
                   </li>
                   <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
