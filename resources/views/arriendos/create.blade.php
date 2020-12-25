ebc6cfd324f0aae3c003910a5524457e7d3b5df9
nattohd <tubepika@gmail.com>
2020-12-25 13:48:21 -0300

    crate

--- resources/views/arriendos/create.blade.php
+++ resources/views/arriendos/create.blade.php
@@ -7,7 +7,43 @@
 <div class="col-lg-8 offset-lg-2 d-flex justify-content-center align-items-center">
     <form class="" method="POST" action="">
         @csrf
-        <h5 class="text-primary">Aqui sigue renato</h5>
+        <div class="card bg-dark px-0 " >
+          <div class="card-header text-light text-center mb-0 pb-0">
+            <h4 style="font-weight: bold"> <i class="fas fa-plus"></i> Crear nueva orden</h4>
+          </div>
+          <div class="card-body mt-0 pt-0">
+              <div class="form-group text-light ">
+                  <label for="rut_cliente" style="color: rgb(255, 255, 255);">Nombre:</label>
+                  <select class="form-control bg-info border-0" name="pais" id="pais" style="color: rgb(14, 13, 13);">
+                    <option value="">RUT CLIENTE</option>
+                    <option value="">RUT CLIENTE 2</option>
+                  </select>                
+              </div>         
+              <div class="row text-light">
+                  {{-- <div class="form-group col-xl-6 col-md-12 text-light">
+                      <label for="id_vehiculo" class="text-light" style="color: rgb(15, 1, 1);">ID Vehiculo:</label>
+                      <input type="text" name="id_vehiculo" class="form-control bg-info border-0 text-light" id="id_vehiculo" placeholder="0001" value="" >
+                  </div> --}}
+                  {{-- VALIDAR LAS FECHAS --}}
+                  <div class="form-group col-xl-6 col-md-12 text-light ">
+                    <label for="arriendo_fecha_inicio" class="text-light" style="color: rgb(15, 1, 1);">Fecha Entrega:</label>
+                    <input type="date" name="arriendo_fecha_inicio" class="form-control bg-info border-0 text-light" id="arriendo_fecha_inicio" placeholder="" value="" >
+                 </div>
+                  <div class="form-group col-xl-6 col-md-12 text-light ">
+                    <label for="arriendo_fecha_final" class="text-light" style="color: rgb(15, 1, 1);">Fecha devolucion :</label>
+                    <input type="date" name="arriendo_fecha_final" class="form-control bg-info border-0 text-light" id="arriendo_fecha_final" placeholder="" value="" >
+                  </div>
+              </div>                                                      
+              <div class="row w-100 m-0 d-flex" >
+                  <div class="col-lg-6 col-12 order-lg-2  order-1 px-0 pb-3 pt-2">
+                    <a href="{{route('arriendos.index')}}" class="btn btn-secondary w-100">Cancelar</a>
+                </div>
+                  <div class=" col-lg-6 col-12 order-lg-2  order-1 px-0 pl-lg-1 pt-2 ">
+                      <button type="submit" class=" btn btn-warning w-100">Siguiente</button>
+                  </div>                                
+              </div>
+          </div>
+      </div>
         
       </form>
 
