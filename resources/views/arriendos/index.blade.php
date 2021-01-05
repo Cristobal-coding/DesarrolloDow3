fc0d708c1e2c57774ff934aec96ffedc801ddd0e
Cristobal-Coding <agartkg95@gmail.com>
2021-01-05 13:52:34 -0600

    fix validarion

--- resources/views/arriendos/index.blade.php
+++ resources/views/arriendos/index.blade.php
@@ -56,60 +56,64 @@
           </tr>
         </thead>
         <tbody class="mi-scrol">
-          @foreach ($arriendos as $arriendo )
-          <tr>
-            <td>{{$arriendo->rut_cliente}}</td>
-            <td>
-              {{$arriendo->cliente->nombre_cliente}}
-            </td>
-            <td>
-              {{$arriendo->usuariovendedor->nombre}} 
-            </td>   
-            <th scope="row">@if($arriendo->confirmada==0) En Curso @else {{$arriendo->estado==1?'Vigente':'Finalizada'}}@endif</th>
-            <td>
-              @if($arriendo->confirmada==0) En Curso @else ${{number_format($arriendo->total,0,".",".")}} CLP @endif
-              
-            </td>   
-            <td class="text-center">
-              <span class="pr-1" data-toggle="tooltip" title="Detalles Orden." data-placement="bottom">                               
-                <a  href="{{route('arriendos.show',$arriendo->id)}}" class="btn btn-success"><i class="fas fa-info-circle fa-lg"></i></a>
-              </span>
+          @if(count($arriendos)!=null)
+            @foreach ($arriendos as $arriendo )
+            <tr>
+              <td>{{$arriendo->rut_cliente}}</td>
+              <td>
+                {{$arriendo->cliente->nombre_cliente}}
+              </td>
+              <td>
+                {{$arriendo->usuariovendedor->nombre}} 
+              </td>   
+              <th scope="row">@if($arriendo->confirmada==0) En Curso @else {{$arriendo->estado==1?'Vigente':'Finalizada'}}@endif</th>
+              <td>
+                @if($arriendo->confirmada==0) En Curso @else ${{number_format($arriendo->total,0,".",".")}} CLP @endif
+                
+              </td>   
+              <td class="text-center">
+                <span class="pr-1" data-toggle="tooltip" title="Detalles Orden." data-placement="bottom">                               
+                  <a  href="{{route('arriendos.show',$arriendo->id)}}" class="btn btn-success"><i class="fas fa-info-circle fa-lg"></i></a>
+                </span>
 
-              <span class="" data-toggle="tooltip" title="Editar." data-placement="bottom">                               
-                <a  href="{{route('arriendos.edit',$arriendo->id)}}" class="btn btn-secondary"><i class="far fa-edit fa-lg"></i></a>
-              </span>
-              <span class="pl-1" data-toggle="tooltip" title="Borrar." data-placement="right">                               
-                <a type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#borrarCliente{{$arriendo->id}}"><i class="fas fa-trash fa-lg"></i></a>
-               </span>  
-               {{-- modal borrar --}}
-               <div class="modal fade text-light " id="borrarCliente{{$arriendo->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                      <div class="modal-content bg-dark" style="color: #fff;">
-                          <div class="modal-header">
-                              <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-trash fa-lg"></i>Confirmar Borrar Arriendo ?</h5>
-                              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true"><i class="fas fa-times border-0" style="color: #fff"></i></span>
-                              </button>
-                          </div>
-                          <div class="modal-body">
-                              <div class="d-flex align-items-center justify-content-center">
-                                  <i class="fas fa-exclamation-triangle fa-lg pr-2 text-warning"></i>¿Desea eliminar el arriendo?
-                              </div>
-                          </div>
-                          <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                              <form method="POST" action="{{route("arriendos.destroy",$arriendo->id)}}">
-                                  @csrf
-                                  @method("delete")
-                                  <button type="submit" data-toggle="tooltip" title="Borrar" data-placement="right" class="btn btn-warning">Borrar Arriendo</button>
-                              </form>
-                          </div>
-                      </div>
-                </div>
-             </div>   
-            </td>
-          </tr>
-          @endforeach        
+                <span class="" data-toggle="tooltip" title="Editar." data-placement="bottom">                               
+                  <a  href="{{route('arriendos.edit',$arriendo->id)}}" class="btn btn-secondary"><i class="far fa-edit fa-lg"></i></a>
+                </span>
+                <span class="pl-1" data-toggle="tooltip" title="Borrar." data-placement="right">                               
+                  <a type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#borrarCliente{{$arriendo->id}}"><i class="fas fa-trash fa-lg"></i></a>
+                </span>  
+                {{-- modal borrar --}}
+                <div class="modal fade text-light " id="borrarCliente{{$arriendo->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                        <div class="modal-content bg-dark" style="color: #fff;">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-trash fa-lg"></i>Confirmar Borrar Arriendo ?</h5>
+                                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true"><i class="fas fa-times border-0" style="color: #fff"></i></span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="d-flex align-items-center justify-content-center">
+                                    <i class="fas fa-exclamation-triangle fa-lg pr-2 text-warning"></i>¿Desea eliminar el arriendo?
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                <form method="POST" action="{{route("arriendos.destroy",$arriendo->id)}}">
+                                    @csrf
+                                    @method("delete")
+                                    <button type="submit" data-toggle="tooltip" title="Borrar" data-placement="right" class="btn btn-warning">Borrar Arriendo</button>
+                                </form>
+                            </div>
+                        </div>
+                  </div>
+              </div>   
+              </td>
+            </tr>
+            @endforeach
+          @else
+          <h5>Aun no se ha añadido nada</h5>   
+          @endif              
         </tbody>
       </table>
     </div>
